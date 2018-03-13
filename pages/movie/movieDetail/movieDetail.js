120806b288b4f756d596ca438c9acda0c2c2cd83
lzccheng <lai.zhicheng@yahoo.com>
2018-03-14 02:16:34 +0800

    update

--- pages/movie/movieDetail/movieDetail.js
+++ pages/movie/movieDetail/movieDetail.js
@@ -6,9 +6,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    err:false
   },
-
+  onLook:function(event){
+    var src = event.currentTarget.dataset.src;
+    wx.previewImage({
+      current:src,
+      urls: [src],
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -24,7 +30,9 @@ Page({
         "Content-Type":"json"
       },
       success:function(res){
-        console.log(res)
+        if (res.statusCode === 404){
+          return that.setData({err:true});
+        }
         var json = {};
         var data = res.data;
         var languagesFn = function(arr){
@@ -33,6 +41,13 @@ Page({
             str += arr[i] + ' / ';
           };
           return str.slice(0, -3);
+        };
+        var name = function(arr){
+          let a = [];
+          for(let i=0;i<arr.length;i++){
+            a.push(arr[i].name);
+          };
+          return a;
         }
         json.average = data.rating.average;
         json.imgSrc = data.images.large;
@@ -41,6 +56,21 @@ Page({
         json.id = data.id;
         json.pubdate = data.pubdate;
         json.languages = languagesFn(data.languages);
+        json.writers = data.writers;
+        json.pubdates = data.pubdates;
+        json.tags = languagesFn(data.tags);
+        json.durations = data.durations;
+        json.aenres = languagesFn(data.genres);
+        json.trailers = data.trailers;
+        json.caste = data.casts;
+        json.countries = languagesFn(data.countries);
+        json.summary = data.summary;
+        json.directors = data.directors;
+        json.directorsStr = languagesFn(name(data.directors));
+        json.comments_count = data.comments_count;
+        json.ratings_count = data.ratings_count;
+        json.arter = languagesFn(name(data.casts));
+        json.writersStr = languagesFn(name(data.writers));
         that.setData({data,json})
       }
     })
